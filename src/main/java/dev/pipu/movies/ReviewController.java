95a9e6f53155802766ee823cb6bf09a476317327
bortcrab <bortcrab@gmail.com>
2024-05-25 13:13:14 -0700

    Creación del repositorio.
    Creé el repositorio de la API de películas. Creo que todavía le falta arreglar errores.

---
+++ src/main/java/dev/pipu/movies/ReviewController.java
@@ -0,0 +1,4 @@
+package dev.pipu.movies;
+
+public class ReviewController {
+}
---
+++ src/test/java/dev/pipu/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pipu.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
